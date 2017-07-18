8412730f6e9981c1fd7606e433ad357eb1193cd5
sfitzgerald16 <misfitz616@gmail.com>
2017-07-17 22:17:20 -0700

    first commit

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,26 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AlbumSchema = require('./album');
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+// UserSchema.pre('remove', function(next) {
+//   const Album = mongoose.model('album');
+//   Album.remove({_id: {$in: this.albums}})
+//     .then(() => next());
+// });
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
